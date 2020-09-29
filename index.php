22aac4caaedb9109f8804bea6b96f4104c190f58
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-29 09:38:08 +0200

    adding html to index page start

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>guest-book challenge</title>
+</head>
+<body>
+
+</body>
+</html>
+
 <?php
 declare(strict_types=1);
-
 
